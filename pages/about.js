cb2dffed2dc05565ab8d2c582e7ca3738cae52b6
Glen112 <escobar.glen@gmail.com>
2024-03-16 17:21:07 -0300

    1-action

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+
+export default function AboutScreen() {
+    return(
+        <div>
+            <h1>Pagina de Sobre</h1>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function HomeScreen() {
+return(
+<div>
+<h1>Pagina de inicio</h1>
+</div>
+)
+}
