d9d647c5441a7df610682de220195885c913746e
Glen112 <escobar.glen@gmail.com>
2024-03-17 05:14:50 -0300

    indent

---
+++ build/_next/static/k6t9vBoeIdXNgocH3nti3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-286d45a326dcd633.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],"/about":["static/chunks/pages/about-34b5dc309cee65f5.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
