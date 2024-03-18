31d7f947e7fbc4febc2868350b43f379abcf4410
Glen112 <escobar.glen@gmail.com>
2024-03-18 19:55:41 -0300

    fetch.3

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -28,8 +28,8 @@ defaultHeaders["accept"] =
   "application/vnd.github.v3+json; application/vnd.github.antiope-preview+json";
 defaultHeaders["content-type"] = "application/json";
 //
-// console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
-// console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
+console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
+console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
 //
 fetch(
   `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
