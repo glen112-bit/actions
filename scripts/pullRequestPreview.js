9761bb58880df9cf9a43e29dee0bd8457acd098f
Glen112 <escobar.glen@gmail.com>
2024-03-18 18:24:54 -0300

    preview.2

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -6,12 +6,12 @@ import { execSync } from "child_process";
 console.log("[DEPLOY_PREVIEW]: START");
 const command = "npm run deploy:staging";
 const output = execSync(command, { encoding: "utf8" });
-// const outputLines = output.split("\n");
-// const DEPLOY_URL = outputLines[outputLines.length - 1];
-console.log(output);
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
+// console.log(output);
 console.log("[DEPLOY_PREVIEW]: END");
 
-// console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
 // ===================================
 // ===================================
